e8da4ea812656c84b8b5ec25ffbe539236d5666f
Jonas Galvez <jonasgalvez@gmail.com>
2019-04-07 13:06:26 -0300

    add benchmark code (3)

--- benchmark/node-native.js
+++ benchmark/node-native.js
@@ -1,17 +1,8 @@
 
+
 const crypto = require('crypto')
-const s = (ms) => ms / 1000
+const { payload, benchmarkFunc } = require('./setup')
 
-let i
-let start
-
-const a = 'jonas galvez'
-i = 0
-start = new Date().getTime()
-for (; i < 1000000; i++) {
-  crypto.createHash('sha256').update(a).digest('hex')
-}
-console.log(s(new Date().getTime() - start))
-
-const used = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(`${Math.round(used * 100) / 100} MB`)
+benchmarkFunc('Native Node', () => {
+  crypto.createHash('sha256').update(payload).digest('hex')
+})
