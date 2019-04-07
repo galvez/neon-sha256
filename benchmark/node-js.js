3cc7b29c4c48524644e5b285829c5ac6b11468c7
Jonas Galvez <jonasgalvez@gmail.com>
2019-04-07 13:06:15 -0300

    add benchmark code (2)

--- benchmark/node-js.js
+++ benchmark/node-js.js
@@ -1,18 +1,8 @@
+
 const CryptoJS = require('crypto-js')
 const SHA256 = require('crypto-js/sha256')
+const { payload, benchmarkFunc } = require('./setup')
 
-const s = (ms) => ms / 1000
-
-let i
-let start
-
-const a = 'jonas galvez'
-i = 0
-start = new Date().getTime()
-for (; i < 1000000; i++) {
-  SHA256(a).toString(CryptoJS.enc.Hex)
-}
-console.log(s(new Date().getTime() - start))
-
-const used = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(`${Math.round(used * 100) / 100} MB`)
+benchmarkFunc('Pure JavaScript', () => {
+  SHA256(payload).toString(CryptoJS.enc.Hex)
+})
